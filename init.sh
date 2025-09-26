ea7dc139538e6e78618afed6527acd6f441cc08e
huyyxy <huyyxy@163.com>
2025-09-26 15:34:45 +0800

    feat: 添加周边相关脚本

---
+++ init.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo aptitude install cmake
+sudo aptitude install gcc
+sudo aptitude install g++
+sudo aptitude install pkgconf
+sudo aptitude install libopencv-core-dev libopencv-highgui-dev libopencv-imgproc-dev libopencv-calib3d-dev libopencv-features2d-dev
+sudo aptitude install libopencv-dev python3-opencv
+sudo aptitude install libeigen3-dev
+sudo aptitude install libgl1-mesa-dev libglew-dev libwayland-dev libxkbcommon-dev wayland-protocols
+sudo aptitude install libegl1-mesa-dev libwayland-dev libxkbcommon-dev wayland-protocols
+sudo aptitude install ffmpeg libavcodec-dev libavutil-dev libavformat-dev libswscale-dev
+sudo aptitude install libpng-dev libjpeg-dev libeigen3-dev
+sudo aptitude install python3-dev python3-pip
+sudo aptitude install libeigen3-dev libopencv-dev cmake gcc g++ git libglew-dev libboost-all-dev
+sudo aptitude install libsigslot-dev
+sudo apt install clang
+sudo aptitude install patchelf
